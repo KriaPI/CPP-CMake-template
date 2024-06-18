4bde012bfee275f4ba3b4024615d003f14f1b0e2
KriaPia <kristoffer.berg8@gmail.com>
2024-06-18 15:53:49 +0200

    Feature: added default compiler flags

--- cmake/defaults.cmake
+++ cmake/defaults.cmake
@@ -1,12 +1,9 @@
-# Set needed 
-macro(project_set_defaults)
-    set(CMAKE_EXPORT_COMPILE_COMMANDS TRUE)    
-
-    if (NOT DEFINED CMAKE_CXX_STANDARD)
-        set(CMAKE_CXX_STANDARD 14)
-        set(CMAKE_CXX_STANDARD_REQUIRED OFF)
-        set(CMAKE_CXX_EXTENSIONS ON)
-    elseif(CMAKE_CXX_STANDARD LESS 14) 
-        message(FATAL_ERROR"This project requires C++ language standard 14 or later")
-    endif()
-endmacro()
+set(CMAKE_EXPORT_COMPILE_COMMANDS TRUE)  
+  
+if (NOT DEFINED CMAKE_CXX_STANDARD)
+    set(CMAKE_CXX_STANDARD 14)
+    set(CMAKE_CXX_STANDARD_REQUIRED OFF)
+    set(CMAKE_CXX_EXTENSIONS ON)
+elseif(CMAKE_CXX_STANDARD LESS 14) 
+    message(FATAL_ERROR"This project requires C++ language standard 14 or later")
+endif()
