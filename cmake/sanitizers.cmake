7ed98c85c1fe309ce909a5dc130e38828a8deeb4
KriaPia <kristoffer.berg8@gmail.com>
2024-06-25 17:08:45 +0200

    Feature: added options for UBSAN and TSAN
    Tested (manually) with Clang version 18.1.3 and GCC 13.2.0. Address
    sanitizer (ASAN) and Undefined Behavior Sanitizer works on both
    compilers. Thread Sanitizer (TSAN) only works on Clang. A check has also been added to
    avoid the use of TSAN and ASAN at the same time. If TSAN and ASAN are
    enabled at the same time, a fatal error will occur.

--- cmake/sanitizers.cmake
+++ cmake/sanitizers.cmake
@@ -1,3 +1,8 @@
+# Options for enabling santizers such as Address Sanitizer (ASAN), Thread Sanitizer (TSAN)
+# and Undefined Behavior Sanitizer (UBSAN). TSAN and UBSAN cannot be enabled at the same time.
+# Note that MSVC only supports ASAN and that TSAN may not work with GCC (version 13.2).
+
+
 option(ENABLE_ASAN "Enable address sanitizer" OFF)
 
 if (CMAKE_CXX_COMPILER_ID MATCHES "MSVC" AND ENABLE_ASAN MATCHES ON)
@@ -9,7 +14,22 @@ if (CMAKE_CXX_COMPILER_ID MATCHES "MSVC" AND ENABLE_ASAN MATCHES ON)
     add_compile_options(
         /fsanitize=address 
     )
-elseif ((CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "GNU") AND ENABLE_ASAN MATCHES ON) 
-    add_link_options(-fsanitize=address)    
-    add_compile_options(-fsanitize=address) 
+elseif ((CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "GNU")) 
+    option(ENABLE_UBSAN "Enable Undefined Behavior Sanitizer (does not work with Address Sanitizer (ASAN))" OFF)
+    option(ENABLE_TSAN "Enable thread sanitizer" OFF)
+
+    if (ENABLE_ASAN MATCHES ON AND ENABLE_TSAN MATCHES ON)
+        message(FATAL_ERROR "Thread Sanitizer (TSAN) and Address Sanitizer (ASAN) cannot be enabled at the same time. Disable one of them to resolve this issue.")
+    endif()
+
+    add_link_options(
+        $<$<BOOL:${ENABLE_ASAN}>:-fsanitize=address>
+        $<$<BOOL:${ENABLE_UBSAN}>:-fsanitize=undefined>
+        $<$<BOOL:${ENABLE_TSAN}>:-fsanitize=thread>
+    )    
+    add_compile_options(
+        $<$<BOOL:${ENABLE_ASAN}>:-fsanitize=address>
+        $<$<BOOL:${ENABLE_UBSAN}>:-fsanitize=undefined>
+        $<$<BOOL:${ENABLE_TSAN}>:-fsanitize=thread>
+    ) 
 endif()
--- source/main.cpp
+++ source/main.cpp
@@ -2,27 +2,41 @@
 
 #include "placeHolder.hpp"
 
+/// This file can be used to test if Clang sanitizers work as intended (ASAN,
+/// LSAN, UBSAN and TSAN).
+
 namespace badPractices {
-    auto leaky() {
-        auto useless {new int{4}};
-    }
+/// @brief Test to see if LSAN (leak sanitizer) reports an error.
+auto leaky() {
+    auto useless{new int{4}};
+    std::cout << "Cool number: " << *useless << '\n';
+}
 
-    auto useAfterFree() {
-        auto freeMemory {new int{4}};
-        delete freeMemory;
+/// @brief Test to see if ASAN (address sanitizer) reports an error.
+auto useAfterFree() {
+    auto freeMemory{new int{4}};
+    delete freeMemory;
 
-        if (*freeMemory == 4) {
-            std::cout << "Infinite memory???" << '\n';
-        }
+    if (*freeMemory == 4) {
+        std::cout << "Infinite memory???" << '\n';
     }
 }
 
+/// @brief Test to see if UBSAN (undefined behavior sanitizer) reports an
+/// runtime error.
+auto undefinedBehavior() {
+    int* coolAddress{nullptr};
+    if (*coolAddress) {
+        std::cout << "wow, that was awesome!" << '\n';
+    }
+}
+}  // namespace badPractices
+
 int main() {
-    //std::cout << "Hi! It seems to work. BTW, 2 + 2 = " << uselessFunction(2, 2)
-     //         << '\n';
-    std::cout << "Hell" << '\n';
+    std::cout << "Nothing?" << '\n';
 
     badPractices::leaky();
-    badPractices::useAfterFree();
+    // badPractices::useAfterFree();
+    // badPractices::undefinedBehavior();
     std::exit(EXIT_SUCCESS);
 }
