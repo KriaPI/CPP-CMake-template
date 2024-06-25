75dff7d7ec2a315a592c04c4124dd58ef23db1d5
KriaPia <kristoffer.berg8@gmail.com>
2024-06-25 11:50:12 +0200

    BugFix: resolved issues with addressSanitizer
    Fixed false positive when compiling with MSVC and addressSanitizer
    turned on. Also fixed linking issue when compiling with Clang and GCC.

--- cmake/sanitizers.cmake
+++ cmake/sanitizers.cmake
@@ -1,11 +1,15 @@
 option(ENABLE_ASAN "Enable address sanitizer" OFF)
 
-if (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
+if (CMAKE_CXX_COMPILER_ID MATCHES "MSVC" AND ENABLE_ASAN MATCHES ON)
+    string(REPLACE "/RTC1" "" CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG}")
     string(REPLACE "/RTC1" "" CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG}")
-    add_compile_options(
-        /fsanitize=address /fsanitize-address-use-after-return
+    add_link_options(
+        /INCREMENTAL:NO   
     )
-elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "GNU") 
-    add_compile_options(-fsanitize=address)
-    add_link_options(-fsanitize=address)
+    add_compile_options(
+        /fsanitize=address 
+    )
+elseif ((CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "GNU") AND ENABLE_ASAN MATCHES ON) 
+    add_link_options(-fsanitize=address)    
+    add_compile_options(-fsanitize=address) 
 endif()
--- source/main.cpp
+++ source/main.cpp
@@ -2,10 +2,27 @@
 
 #include "placeHolder.hpp"
 
+namespace badPractices {
+    auto leaky() {
+        auto useless {new int{4}};
+    }
+
+    auto useAfterFree() {
+        auto freeMemory {new int{4}};
+        delete freeMemory;
+
+        if (*freeMemory == 4) {
+            std::cout << "Infinite memory???" << '\n';
+        }
+    }
+}
 
 int main() {
-    std::cout << "Hi! It seems to work. BTW, 2 + 2 = " << uselessFunction(2, 2)
-              << '\n';
+    //std::cout << "Hi! It seems to work. BTW, 2 + 2 = " << uselessFunction(2, 2)
+     //         << '\n';
+    std::cout << "Hell" << '\n';
 
+    badPractices::leaky();
+    badPractices::useAfterFree();
     std::exit(EXIT_SUCCESS);
 }
